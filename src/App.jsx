a36e6d4f10562b0ba7379083803e14105eaec0ce
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-15 11:40:09 -0300

    pagina inicial feita

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 
 import catAndDogImage from './assets/cat-and-dog-animate.svg'
+import amiPetsLogo from './assets/amipets-logo.svg'
 
 import './App.css'
 
@@ -12,8 +13,10 @@ function App() {
     <div className="App">
       <Header />
       <section>
-        <h1>amiPets</h1>
-        <a href="#">Ver pets disponíveis para adoção</a>
+        <h1>
+          <img src={amiPetsLogo} alt="amiPets Logo" />
+        </h1>
+        <a href="#" className='link'>Ver pets disponíveis para adoção</a>
         <img src={catAndDogImage} alt="Cat and Dog animated" />
       </section>
     </div>
