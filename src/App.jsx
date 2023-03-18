2af47be3560e2fa38c7d6b0d2f64e497f83ff84e
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-18 16:33:04 -0300

    criação de contexto

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 import { Routes, Route } from 'react-router-dom'
+import { useState } from "react"
+
 import Home from "./pages/Home"
 import Pets from "./pages/Pets"
 import Detalhes from "./pages/Detalhes"
-import Footer from "./components/Footer"
+
+import { ThemeContext } from "./context/ThemeContext"
 
 function App() {
-  
+  const [theme, setTheme] = useState('light')
+
+  function toggleTheme() {
+    setTheme((curr) => curr === 'light' ? 'dark' : 'light')
+  }
 
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/pets" element={<Pets />} />
-        <Route path="/detalhes/:id" element={<Detalhes />} />
-      </Routes>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/pets" element={<Pets />} />
+          <Route path="/detalhes/:id" element={<Detalhes />} />
+        </Routes>
+      </ThemeContext.Provider>
       <Footer />
     </div>
   )
